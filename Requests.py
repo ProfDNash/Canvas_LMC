111858379c4a287418d7e18d223be2a795eaec47
David A. Nash <47865752+ProfDNash@users.noreply.github.com>
2020-12-14 19:47:06 -0500

    added autodetect/remove faculty
    Added functionality to detect faculty IDs and remove any requests from those IDs.

--- Requests.py
+++ Requests.py
@@ -19,10 +19,23 @@ We also will use -1 as a replacement for \ N in those keys so they can be stored
 as integers instead of strings.'''
 
 KEYDIFF = 15270000000000000
-##List of known faculty user_ids
+##List of known faculty user_ids -- this includes anyone who created new
+##assignments, etc.
 faculty = [454254935450860033,352329503483327465,461510280206911953,
            260399413616254107,446354931750470913,459297289574575361,
-           13494308845404191,-111579948995598634,-439233688573536041]
+           13494308845404191,-111579948995598634,-439233688573536041,
+           -331983840839712843,279149467034293695,168521247232947848,
+           -276503210160044986,22038591728744718,-262710809639375872,
+           -20556520449892440,207578437277249390,449545178198982231,
+           269525800159524778,433449880292367858,169297019236742547,
+           383696511811241783,-444968307030906019,-129125899578755545,
+           -129924333674845945,-513414722028092034,528368423185167393,
+           134794813544496452,349032263021651377,-160041951289911943,
+           -331983840839712843,-390256345561288500,213550622228073392,
+           -53469270470739886,-189986813610458687,-189007000015170712,
+           218988151122579889,363382961312611750,221590282248879275,
+           555427588082431840,-533044698678973262,-520779095311242509,
+           -66791551983613735]
 
 def readRequests(): ##generate pandas DataFrame and update SQL db
     ##define column headers account_dim Canvas flat file
@@ -38,8 +51,11 @@ def readRequests(): ##generate pandas DataFrame and update SQL db
     
     path = 'C:/Users/ProfN/Documents/GitHub/Canvas_LMC/Canvas_LMC/Dec 10 Data/'
     file_name = []
+    requests_agg = []
+    requests_full = []
     for file in os.listdir(path):
         if file.endswith('.gz') and 'requests' in file:
+            print('Reading data from file',file)
             file_name.append(file)
             '''Lots of work to do here... this is always going to be
             the biggest file and with the most updating to do
@@ -84,48 +100,67 @@ def readRequests(): ##generate pandas DataFrame and update SQL db
             only instructors and IT can masquerade as other users, so
             we remove all rows with REAL_USER_ID not null, and drop that col.
             Moreover, if USER_ID is missing, we drop that request
-            We also drop requests from known faculty IDs
             '''
             temp = temp[temp.developer_key_id==r'\N']
             temp = temp[temp.real_user_id==r'\N']
             temp.drop(columns=['developer_key_id', 'real_user_id'],
                       inplace=True)
             temp = temp[temp.user_id!=r'\N'] ##drop requests with no user_id
-            temp['fac'] = temp.user_id.apply(lambda x: x in faculty)
-            temp = temp[temp.fac!=True]
-            temp.drop(columns=['fac'], inplace=True)
+            ##convert data type
+            temp.user_id = temp.user_id.astype('int64')
+
+
             
             '''
             if URL contains 'undefined', then the request went to a missing
             page.  For now, we drop all such rows as they should not count
-            Moreover, if URL contains 'preview' then it must be faculty.
+            Moreover, if URL contains 'gradebook' then it must be faculty.
+            So use those values to generate a list of faculty ids
+            Drop those faculty rows and those actions
             '''
-            temp['test'] = temp.url.apply(lambda x: 'undefined' in x)
-            temp = temp[temp.test==False]
-            temp['test2'] = temp.url.apply(lambda x: 'preview' in x)
-            temp = temp[temp.test2==False]
-            temp.drop(columns=['test', 'test2'],inplace=True)
+            temp['und'] = temp.url.apply(lambda x: 'undefined' in x
+                                         or 'users' in x)
+            temp = temp[temp.und!=True]
+
+            temp['test'] = temp.url.apply(lambda x: 'gradebook' in x)
+            fac_list = list(temp[temp.test].user_id.drop_duplicates())
+            fac_list = [int(x) for x in fac_list]
+            print('\nFaculty detected:', len(fac_list))
+            ##add in known faculty
+            fac_list = set(fac_list+faculty)
+            ##drop rows with faculty ids
+            temp['fac'] = temp.user_id.apply(lambda x: x in fac_list)
+            #print(temp.fac.sum())
+            temp = temp[temp.fac!=True]
+            temp.drop(columns=['fac', 'test','und'], inplace=True)
+            #temp = temp[temp.test==False]
+            #temp.drop(columns=['test'],inplace=True)
             
             
             '''
             Create a list of WEB_APPLICATION_ACTION values that
             cannot possibly correspond to student actions
             supposedly ping corresponds to idle activity
+            activity_stream_summary seems to also correspond to idleness
+            Also drop 'quiz_assignment_overrides' from WEB_APPLICATION_CONTROLLER
             '''
             non_student = ['save_assignment_order','publish','unpublish',
                            'speed_grader_settings','student_view','backup',
                            'turnitin_report','reorder_items','speed_grader',
                            'moderate','ping','change_gradebook_version',
-                           'change_gradebook_column_size',
+                           'change_gradebook_column_size','new','unenroll_user',
                            'save_gradebook_column_order','reorder',
                            'reorder_assignments', 'move_questions',
-                           'extensions']
+                           'extensions','activity_stream_summary']
             temp['non'] = temp.web_application_action.apply(lambda x:
                                                             False if x in
                                                             non_student
                                                             else x)
             temp = temp[temp.non!=False]
-            temp.drop(columns=['non'], inplace=True)
+            temp['over'] = temp.web_application_controller.apply(
+                lambda x: 'overrides' in x)
+            temp = temp[temp.over!=True]
+            temp.drop(columns=['non','over'], inplace=True)
             
             
             '''
@@ -142,7 +177,11 @@ def readRequests(): ##generate pandas DataFrame and update SQL db
             
             
             ##return requests without aggregation for further exploration
-            requests_full = temp.copy()  
+            if len(requests_full)==0:
+                requests_full = temp.copy()
+            else:
+                requests_full = requests_full.append(temp.copy())
+                requests_full.drop_duplicates(inplace=True)
             
             
             
@@ -186,7 +225,7 @@ def readRequests(): ##generate pandas DataFrame and update SQL db
             ##change datatypes to ints
             temp.hourly_views = temp.hourly_views.astype(int)
             temp.hourly_actions = temp.hourly_actions.astype(int)
-            requests_agg = temp
+            requests_agg.append(temp)
         else:
             pass
 
